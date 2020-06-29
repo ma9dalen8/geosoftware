bb52904e32ff66403a6afae11e4db00d20b5424b
ma9dalen8 <61787839+ma9dalen8@users.noreply.github.com>
2020-06-29 13:30:09 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,23 @@
+<h1><strong>Geosoftware 1 - Exercise 5</strong></h1>
+<h4><strong> by Magdalena Fischer</strong></h4>
+<p> This Repository contains all the scripts for the exercise 5 for the Geosoftware 1 course.<br/>
+    The project has two HTML pages. On the first page all the busstops from Münster are displayed in a map.
+    If they are clicked, the next busconnections will be shown in the table below. 
+    Also all the Objects can be loaded from the Mongodb database. If there are already some entries, the distance to the next Busstop can be calculated.
+    <br/>
+    On the second HTML page- a user can enter locations. Whether using an adress, the browser location or dropping a marker.
+    All Points will be safed in the Database. After loading the Database to the Website again, single or multiple Objects can be deleted.
+    Using the Update button, after checking one Object, loads the coordinates to an textarea so they can be changed. Afterwards they are updated in the database.</p>
+    
+<h2>Getting Started</h2>
+<p> I used npm express and Mongodb for that project. Installing the server, set the main page to "mainIndex.html" and the start scripts to "node start.js"
+    <br/><strong>Packages</strong><br/>
+    npm install --express<br/>
+    npm install --mongodb<br/>
+    npm install --body-parser<br/>
+    npm install --jquery<br/><br/>
+    For the mapping and the geocoding I used Mapbox.<br/>
+    --> Please fill your accessTokens into the scripts. Twice in "usermap.js", once in "map.js"
+    </p>
+<h2></h2>
+<p></p>
---
+++ public/map.js
@@ -0,0 +1,221 @@
+/**
+*@author Magdalena Fischer
+*m_fisc39@wwu.de
+*matr.Nr.: 460 858
+*08.06.2020
+*/
+
+//****various Linter configs****
+// jshint esversion: 6
+// jshint browser: true
+// jshint node: true
+// jshint -W097
+
+//Creating Map with mapbox, center it to Münster
+//source: https://leafletjs.com/examples/quick-start/
+//######Please add your own accessToken########
+
+var busstopInfos= [];
+var basemap = L.map('mapid', {
+  center:[51.96225013358843,7.62451171875],
+  zoom:13});
+var data = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+//#######please fill in your accessToken########################################
+    accessToken: 'pk.eyJ1IjoibWE5ZGFsZW44IiwiYSI6ImNrYTZ4ZGdqNDBibWUyeHBuN3JmN2lrdDcifQ.SgZHAThfZLyx2Avk3th2Lg'
+}).addTo(basemap);
+
+/**
+*@function mappingBusstops
+*@desc creating for each point in the Busstop GeoJSON a circle on the map
+*
+*/
+var heatCoordinates=[];
+var allBusstops;
+function mappingBusstops(){
+
+
+  allBusstops=L.geoJSON(JSON.parse(x.responseText),{
+   pointToLayer: function(features, latlng){
+     heatCoordinates.push([parseFloat(latlng.lat),parseFloat( latlng.lng), 0.5]);
+     return L.circle(latlng);
+   },
+   //creating popup with the BusstopId und Busstopname
+   onEachFeature: function(feature, layer){
+     departureTimeResults=feature.properties.nr;
+     layer.bindPopup('<strong>'+feature.properties.lbez+'</strong> <br/>'+departureTimeResults );
+     //console.log(latlng)
+
+   }
+ }).addTo(basemap).on('click', onClick); // eventlistener click on Circle event
+ //var heat =L.heatLayer(allBusstops) .addTo(basemap).on('click', onClick); // eventlistener click on Circle event
+ var heat=new L.heatLayer(heatCoordinates, {radius:75});//.addTo(basemap);
+   //heatCoordinates, {radius: 35}).addTo(basemap);
+ console.log(heat);
+ var baseLayer={ basemap };
+ var overlays={ heat};
+ L.control.layers(null, overlays).addTo(basemap);
+
+}
+/* {
+  color: 'blue',
+  fillColor: '#f03',
+  fillOpacity: 0.5,
+  radius: 2
+}),{radius: 25}).addTo(basemap);*/
+/*
+allBusstops= L.heatLayer([geoJSON(JSON.parse(x.responseText),{
+  pointToLayer: function(features, latlng){
+    return L.circle(latlng,0.2);
+}})], {radius: 25}).addTo(basemap);
+
+for(var i=0; i<pointCollection.features.length; i++){
+busstopInfos.push(pointCollection.features[i].geometry.coordinates);
+L.heatLayer([busstopInfos[i][1], busstopInfos[i][0],0.2],{radius: 25}).addTo(basemap);
+
+ allBusstops=L.geoJSON(JSON.parse(x.responseText),{
+  pointToLayer: function(features, latlng){
+    return L.circle(latlng);
+  },
+  //creating popup with the BusstopId und Busstopname
+  onEachFeature: function(feature, layer){
+    departureTimeResults=feature.properties.nr;
+    layer.bindPopup('<strong>'+feature.properties.lbez+'</strong> <br/>'+departureTimeResults );
+    departureTimeResults=[];
+  }
+}).addTo(basemap).on('click', onClick); // eventlistener click on Circle event
+}
+*/
+/**
+*@function onClick
+*@desc waits for an click event, starts the departureTime Calculation for clicked Busstop
+*/
+var popup;
+var busstopName;
+
+function onClick(e){
+  popup = e.latlng;
+  var businfos= e.layer.getPopup();
+  basemap.closePopup();
+
+  businfos=businfos._content;
+  // saving Popupinfos in variables
+  var busstopId= businfos.slice(-7);
+  busstopName= businfos.slice(0,-7);
+  //console.log(busstopId);
+  //console.log(busstopName);
+
+  departureTimes(busstopId);
+  busstopId='';
+
+}
+
+/**
+*@function showingDatabaseContent
+*@desc shows the MongoDB database content
+*/
+
+async function showingDatabaseContent(){
+  let result =await promise();
+  checkboxList(result);
+}
+
+/**
+*@function promise
+*@desc sends a request to the server via /item
+*/
+
+function promise(){
+
+  return new Promise(function (res, req){
+    $.ajax({
+      url:"/item",
+      success: function (result){res(result);},
+      error: function (err) {console.log(err);}
+    });
+  });
+}
+
+/**
+*@function checkboxList
+*@desc creates a checkbox (radio button) list with all database objects and shows it on the main-webpage
+*/
+
+var datalength;
+function checkboxList(result){
+  datalength=result.length;
+  //creates unordered radio button list with all elements of the database
+  for(var i=0; i<result.length; i++){
+    //console.log(result[i]);
+    var ul = document.getElementById('ul');
+    var li = document.createElement('li'+i);
+
+    var checkbox = document.createElement('input');
+    checkbox.type = "radio";
+    checkbox.id = "checkboxid" + i;
+    checkbox.value=JSON.stringify(result[i]);
+    checkbox.name="location";
+    //console.log(checkbox.name);
+
+    //adds all elements to the website
+    li.appendChild(checkbox);
+    li.appendChild(document.createTextNode(JSON.stringify(result[i])));
+    ul.appendChild(li);
+    ul.appendChild(document.createElement("br"));
+    ul.appendChild(document.createElement("br"));
+  }
+}
+
+/**
+*@function getValue
+*@desc if necessary converts data of checked item to float values and calls startCalculation function
+*/
+var choseninputMarker;
+function getValue(){
+  var value=[];
+  var databaseCoordinates;
+  //saving the checked item in a variable
+  for(var i =0; i<datalength;i++){
+    if(document.getElementById("checkboxid"+i).checked){
+      value=document.getElementById("checkboxid"+i).value;
+      value=JSON.parse(value);
+      databaseCoordinates=value.geometry.coordinates;
+
+      //if the coordinates are a string--> transforms it to a float
+      if(typeof databaseCoordinates[0] === "string"){
+
+        databaseCoordinates=databaseCoordinates[0];
+        databaseCoordinates=databaseCoordinates.split(',');
+        //console.log(databaseCoordinates);
+        parseFloat(databaseCoordinates[0]);
+        parseFloat(databaseCoordinates[1]);
+        //console.log(databaseCoordinates);
+        inputLocation=databaseCoordinates;
+        mappingUserInput(inputLocation);
+        startCalculation();
+    }
+      else{
+        inputLocation=value.geometry.coordinates;
+         //console.log(inputLocation);
+      startCalculation();
+      mappingUserInput(inputLocation);
+    }}
+
+  }
+  //console.log(document.getElementById("checkboxid1").checked);
+}
+/**
+*@function mappingUserInput
+*@desc displaying User Input adress on the draw Map, Mapbox
+*/
+var inputMarker={};
+function mappingUserInput (convertedAdress){
+  inputMarker.length=0;
+   inputMarker =L.marker([convertedAdress[1], convertedAdress [0]], {
+
+  }).addTo(basemap);
+}
